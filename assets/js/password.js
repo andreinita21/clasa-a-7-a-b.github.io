d4643d5b8b697320280f0a6254dbf2ef25bece6f
Minionul2006 <minionul2006@users.noreply.github.com>
2020-03-28 18:26:01 +0200

    Password added

---
+++ assets/js/password.js
@@ -0,0 +1,15 @@
+<script language="Javascript">
+<!--hide
+
+var password;
+var pass="cnaic2020"
+password=prompt('Please enter the password to get acces to this website','');
+
+if(password==pass)
+alert('Correct Password, press the "OK" button');
+else
+{
+window.location="https://clasa-a-7-a-b.github.io/";
+}
+//-->
+</script>
